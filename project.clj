3add1dfc1206aa280ed0ec9bca9b8a8380ad9837
Alexander Oloo <alekcz@gmail.com>
2017-06-16 18:08:57 +0200

    add url to repo to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject alekcz/charmander "0.1.0"
   :description "Charmander: a set of libraries to make working with firebase easier in clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/alekcz/charmander"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [ [org.clojure/clojure "1.8.0"]
