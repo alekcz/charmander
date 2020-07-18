83b304d3a285fe82d89c0423576cf410391a4277
Alexander Oloo <alekcz@gmail.com>
2020-07-18 09:11:15 +0200

    v1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alekcz/charmander "1.0.2-SNAPSHOT"
+(defproject alekcz/charmander "1.0.2"
   :description "Charmander: a set of libraries to make working with firebase easier in clojure"
   :url "https://github.com/alekcz/charmander"
   :license {:name "Eclipse Public License"
